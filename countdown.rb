d189465d8860b35b731063527e668f7fa4e392f8
David Ingram <david@ingramaviation.com>
2019-08-13 18:25:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,12 @@ def countdown(number_seconds)
  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep()
+def countdown_with_sleep(number_seconds)
+  number_seconds = 10 
+  while number_seconds > 0 
+  puts "#{number_seconds} SECOND(S)!"
+  sleep 2 
+  number_seconds -= 1
+ end
+ return "HAPPY NEW YEAR!"
+end
